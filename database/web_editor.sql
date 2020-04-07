b64526ce3e806e8a5f46d566cbd16b61ccca4a3e
lucamaina <mainaluca96@gmail.com>
2020-04-07 14:24:14 +0200

    db

--- database/web_editor.sql
+++ database/web_editor.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Lug 14, 2019 alle 17:48
+-- Generation Time: Apr 07, 2020 alle 14:22
 -- Versione del server: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `web_editor`
 --
-CREATE DATABASE IF NOT EXISTS `web_editor` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `web_editor`;
 
 -- --------------------------------------------------------
 
@@ -32,7 +30,19 @@ CREATE TABLE IF NOT EXISTS `documenti` (
 `DocId` int(11) NOT NULL,
   `FileSysPath` varchar(128) NOT NULL,
   `LinkPath` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
+
+--
+-- Dump dei dati per la tabella `documenti`
+--
+
+INSERT INTO `documenti` (`DocId`, `FileSysPath`, `LinkPath`) VALUES
+(1, 'file.txt', 'mioFile'),
+(2, 'file1.txt', 'asd_file1'),
+(3, 'file2.txt', 'asd_file2'),
+(4, 'file_remoto.txt', 'file_remoto.txt'),
+(22, 'asd', 'asd'),
+(23, 'nuovoF', 'nuovoF');
 
 -- --------------------------------------------------------
 
@@ -41,10 +51,22 @@ CREATE TABLE IF NOT EXISTS `documenti` (
 --
 
 CREATE TABLE IF NOT EXISTS `relazioni` (
-  `RId` int(11) NOT NULL,
-  `UserId` int(11) NOT NULL,
-  `DocId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+`RId` int(11) NOT NULL,
+  `DocId` int(11) NOT NULL,
+  `UserName` varchar(64) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dump dei dati per la tabella `relazioni`
+--
+
+INSERT INTO `relazioni` (`RId`, `DocId`, `UserName`) VALUES
+(1, 1, 'user'),
+(2, 22, 'user'),
+(3, 1, 'user1'),
+(4, 22, 'user1'),
+(5, 1, 'asd'),
+(6, 23, 'user');
 
 -- --------------------------------------------------------
 
@@ -53,18 +75,22 @@ CREATE TABLE IF NOT EXISTS `relazioni` (
 --
 
 CREATE TABLE IF NOT EXISTS `utenti` (
-`UserId` int(11) NOT NULL,
   `UserName` varchar(64) NOT NULL,
   `NickName` varchar(32) NOT NULL,
-  `PasswordCript` varchar(32) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `Password` varchar(64) NOT NULL,
+  `connesso` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `utenti`
 --
 
-INSERT INTO `utenti` (`UserId`, `UserName`, `NickName`, `PasswordCript`) VALUES
-(1, 'user1@asd.it', 'user1', '0');
+INSERT INTO `utenti` (`UserName`, `NickName`, `Password`, `connesso`) VALUES
+('asd', 'asd', 'dd121e36961a04627eacff629765dd3528471ed745c1e32222db4a8a5f3421c4', 0),
+('pippo', 'pippo', 'dd121e36961a04627eacff629765dd3528471ed745c1e32222db4a8a5f3421c4', 0),
+('qwe', 'qwe', 'dd121e36961a04627eacff629765dd3528471ed745c1e32222db4a8a5f3421c4', 0),
+('user', 'user', 'dd121e36961a04627eacff629765dd3528471ed745c1e32222db4a8a5f3421c4', 0),
+('user1', 'user1', 'dd121e36961a04627eacff629765dd3528471ed745c1e32222db4a8a5f3421c4', 0);
 
 --
 -- Indexes for dumped tables
@@ -74,19 +100,19 @@ INSERT INTO `utenti` (`UserId`, `UserName`, `NickName`, `PasswordCript`) VALUES
 -- Indexes for table `documenti`
 --
 ALTER TABLE `documenti`
- ADD PRIMARY KEY (`DocId`);
+ ADD UNIQUE KEY `DocId` (`DocId`);
 
 --
 -- Indexes for table `relazioni`
 --
 ALTER TABLE `relazioni`
- ADD PRIMARY KEY (`RId`,`UserId`,`DocId`);
+ ADD UNIQUE KEY `RId` (`RId`), ADD KEY `docid` (`DocId`), ADD KEY `username` (`UserName`);
 
 --
 -- Indexes for table `utenti`
 --
 ALTER TABLE `utenti`
- ADD PRIMARY KEY (`UserId`);
+ ADD PRIMARY KEY (`UserName`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -96,12 +122,12 @@ ALTER TABLE `utenti`
 -- AUTO_INCREMENT for table `documenti`
 --
 ALTER TABLE `documenti`
-MODIFY `DocId` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `DocId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
 --
--- AUTO_INCREMENT for table `utenti`
+-- AUTO_INCREMENT for table `relazioni`
 --
-ALTER TABLE `utenti`
-MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+ALTER TABLE `relazioni`
+MODIFY `RId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
